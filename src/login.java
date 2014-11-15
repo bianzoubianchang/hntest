1dfde76eed70f4804be9bf209695a27865dd8ebd
longyu <yqcui669@126.com>
2014-11-15 16:37:37 +0800

    Edit login.java

--- src/login.java
+++ src/login.java
@@ -20,7 +20,7 @@ public class login{
 	@DataProvider(name="loginErrorAttempt")
 	public Object[][] loginError(){
 		return new Object[][]{
-				{"19900000001","111112","111111"},
+				{"19900000002","111112","111111"},
 		};
 	}
 	
