b627e01a517152981ee847103a71a3aa3bc0cdaa
longyu <yqcui669@126.com>
2014-11-20 15:25:01 +0800

    add infomation.

--- src/login.java
+++ src/login.java
@@ -1,5 +1,16 @@
 
+/*
+ * 脚本说明：
+ * 该脚本主要用于验证登录错误次数限制
+ * 
+ * 功能点：
+ * 1.允许错误登录5次，超过2次错误登录后提示剩余的错误登录次数
+ * 2.错误登录达5次后，冻结账户
+ * 3.正确登录后，恢复错误登录的次数
+ */
+
 import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,6 +28,7 @@ public class login{
 	private WebDriver driver;
 	
 
+	//指定测试账号数据源：用户名、错误密码和正确密码
 	@DataProvider(name="loginErrorAttempt")
 	public Object[][] loginError(){
 		return new Object[][]{
@@ -39,9 +51,14 @@ public class login{
 	@Test(invocationCount=1,dataProvider="loginErrorAttempt")
 
 	public void userlogin(String username_para,String password,String true_password) throws Exception{
+		
+		System.out.println("****验证错误登录的次数限制****");
+		System.out.println("测试数据说明如下：");
+		System.out.println("用户名："+username_para+"，错误密码："+password+"，正确密码："+true_password);
 
 		String usernameTooltip=driver.findElement(By.id("txt_username")).getAttribute("placeholder");
 		String passwordTooltip=driver.findElement(By.id("txt_pwd")).getAttribute("placeholder");
+		//验证用户名和密码文本框的默认提示	
 		Assert.assertEquals("用户名为手机号", usernameTooltip);
 		Assert.assertEquals("6-20位密码", passwordTooltip);
 		
@@ -54,6 +71,7 @@ public class login{
 			driver.findElement(By.id("txt_pwd")).clear();
 			
 			if(i==5 && n==0){
+			//第5次登录成功后，恢复尝试登录次数	
 			driver.findElement(By.id("txt_pwd")).sendKeys(true_password);
 			Thread.sleep(5000);	
 			driver.findElement(By.id("greenSubmit")).click();
@@ -64,6 +82,7 @@ public class login{
 			System.out.println("-----------------------");
 			System.out.println("-----------------------");
 			
+			//登录成功后，重新进行错误登录，验证错误登录5次后的提示
 			n++;
 			i=0;
 			continue;
@@ -74,7 +93,8 @@ public class login{
 			}
 			
 	
-			Thread.sleep(2000);			
+			//获取并打印每次错误登录的提示信息	
+			Thread.sleep(2000);	
 			String tooltips=driver.findElement(By.xpath("//html/body/div[3]/div[2]/div[1]/div[2]/div")).getText();
 			
 			System.out.println("第"+i+"次错误登录的提示");
